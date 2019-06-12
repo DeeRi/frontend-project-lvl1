1ea7940942e2f9d74d4d0fcd053fc052ab2f46d7
Alena Zueva <aleo.zueva@gmail.com>
2019-06-12 18:09:21 +0700

    add eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import putName from '..';
+
 console.log('Welcome to the Brain Games!');
 
 putName();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const putName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-};
+function putName() {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+}
 
 export default putName;
