aefad9b209c7f919c34055feab3c03c54d46085f
Alena Zueva <aleo.zueva@gmail.com>
2019-06-20 15:47:55 +0700

    game code removed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 /* eslint-disable no-eval */
 import readlineSync from 'readline-sync';
 
-// greeting
-
 export function makeGreeting() {
   console.log('Welcome to the Brain Games!');
   const nameOfUser = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${nameOfUser}!`);
 }
-
-// helping functions
 
 export function getRandom() {
   const max = 100;
@@ -53,7 +49,8 @@ export function greatestDivisor(num1, num2) {
   return greatestDivisor(num2, num1 % num2);
 }
 
-let hiddenNumber;
+// eslint-disable-next-line import/no-mutable-exports
+export let hiddenNumber;
 export function initSeries() {
   let x = 0;
   let resultString = '';
@@ -63,7 +60,6 @@ export function initSeries() {
   const max = 10;
   let index = min + Math.random() * (max + 1 - min);
   index = Math.floor(index);
-
   for (let i = 1; i <= 10; i += 1) {
     x += step;
     const isIndex = i === index ? '..' : `${x}`;
@@ -75,7 +71,7 @@ export function initSeries() {
   return resultString;
 }
 
-function isPrime(number) {
+export function isPrime(number) {
   // eslint-disable-next-line no-restricted-globals
   if (isNaN(number) || !isFinite(number) || number % 1 || number < 2) { return false; }
   const max = Math.floor(Math.sqrt(number));
@@ -85,141 +81,4 @@ function isPrime(number) {
     }
   }
   return true;
-}
-
-
-// games
-
-export function randomNumberGame() {
-  console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if number even otherwise answer "no".');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-
-  let i = 0;
-  while (i < 3) {
-    const gameFunction = getRandom();
-    console.log(`Question: ${gameFunction}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if ((gameFunction % 2 === 0 && userAnswer === 'yes') || (gameFunction % 2 !== 0 && userAnswer === 'no')) {
-      console.log('Correct!');
-      i += 1;
-    }
-    if ((gameFunction % 2 !== 0 && userAnswer === 'yes')) {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.Let's try again, ${userName}!`);
-      return;
-    }
-
-    if ((gameFunction % 2 === 0 && userAnswer === 'no')) {
-      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-}
-
-
-export function countNumberGame() {
-  console.log('Welcome to the Brain Games!');
-  console.log('What is the result of the expression?');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-
-  let i = 0;
-  while (i < 3) {
-    const gameFunction = randomExpression();
-    console.log(`Question: ${gameFunction}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-
-    if (Number(userAnswer) === eval(gameFunction)) {
-      console.log('Correct!');
-      i += 1;
-    } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${eval(gameFunction)}.Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-}
-
-
-export function greatestDivisorGame() {
-  console.log('Welcome to the Brain Games!');
-  console.log('Find the greatest common divisor of given numbers.');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-
-
-  let i = 0;
-  while (i < 3) {
-    const num1 = getRandom();
-    const num2 = getRandom();
-    console.log(`Question: ${num1} ${num2}`);
-    const rightAnswer = greatestDivisor(num1, num2);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (Number(userAnswer) === rightAnswer) {
-      console.log('Correct!');
-      i += 1;
-    } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-}
-
-export function initSeriesGame() {
-  console.log('Welcome to the Brain Games!');
-  console.log('What number is missing in the progression?');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-
-  let i = 0;
-  while (i < 3) {
-    const gameFunction = initSeries();
-    console.log(`Question: ${gameFunction}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (hiddenNumber === Number(userAnswer)) {
-      console.log('Correct!');
-      i += 1;
-    } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${hiddenNumber}.Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-}
-
-
-export function isPrimeGame() {
-  console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-
-  let i = 0;
-  while (i < 3) {
-    const gameFunction = getRandom();
-    console.log(`Question: ${gameFunction}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if ((isPrime(gameFunction) === true && userAnswer === 'yes') || (isPrime(gameFunction) === false && userAnswer === 'no')) {
-      console.log('Correct!');
-      i += 1;
-    }
-    if (isPrime(gameFunction) === false && userAnswer === 'yes') {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.Let's try again, ${userName}!`);
-      return;
-    }
-
-    if (isPrime(gameFunction) === true && userAnswer === 'no') {
-      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
 }
