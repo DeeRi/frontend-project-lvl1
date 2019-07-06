585dcf9fc6b1322fa08f6e860f86904a6809d7f0
Alena Zueva <aleo.zueva@gmail.com>
2019-07-06 10:55:54 +0700

    naming changed

--- src/games/even.js
+++ src/games/even.js
@@ -2,13 +2,13 @@ import { cons } from 'hexlet-pairs';
 import getRandomValue from '../random';
 import playGame from '..';
 
-const isEvenValue = number => number % 2 === 0;
+const isEven = number => number % 2 === 0;
 
 const gameDescription = 'Answer "yes" if number even otherwise answer "no".';
 
 const getGameData = () => {
   const gameQuestion = getRandomValue(1, 100);
-  const rightAnswer = isEvenValue(gameQuestion) ? 'yes' : 'no';
+  const rightAnswer = isEven(gameQuestion) ? 'yes' : 'no';
   return cons(gameQuestion, rightAnswer);
 };
 
