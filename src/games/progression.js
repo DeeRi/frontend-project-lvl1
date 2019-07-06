9d7d6fb31971ebcd7d7fa671f1432e1d4840415d
Alena Zueva <aleo.zueva@gmail.com>
2019-07-06 12:14:39 +0700

    separate function for making progression added

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,18 +5,26 @@ import playGame from '..';
 const gameDescription = 'What number is missing in the progression?';
 const progressionLength = 10;
 
+const makeProgression = (firstElement, step, hiddenElementIndex) => {
+  const result = '';
+  const iter = (index, acc, progressionElement) => {
+    if (index === progressionLength - 1) {
+      return acc;
+    }
+    if (index === hiddenElementIndex) {
+      return iter(index + 1, `${acc} ..`, progressionElement + step);
+    }
+    return iter(index + 1, `${acc} ${progressionElement}`, progressionElement + step);
+  };
+  return iter(0, result, firstElement);
+};
+
 const getGameData = () => {
-  const firstElement = getRandomValue(1, 20);
-  const step = getRandomValue(1, 100);
+  const firstElement = getRandomValue(0, 20);
+  const step = getRandomValue(1, 50);
   const hiddenElementIndex = getRandomValue(0, progressionLength - 1);
   const rightAnswer = String(firstElement + step * hiddenElementIndex);
-  let gameQuestion = '';
-
-  for (let i = 0; i < progressionLength; i += 1) {
-    const newElement = firstElement + step * i;
-    const isSameIndex = hiddenElementIndex === i ? '..' : newElement;
-    gameQuestion = `${gameQuestion} ${isSameIndex}`;
-  }
+  const gameQuestion = makeProgression(firstElement, step, hiddenElementIndex);
   return cons(gameQuestion, rightAnswer);
 };
 
