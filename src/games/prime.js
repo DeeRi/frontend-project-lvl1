f9dc8f445e3cccafa7763799a3f09c7c7fcda71f
Alena Zueva <aleo.zueva@gmail.com>
2019-07-06 11:21:39 +0700

    naming changed; isPrime function improved

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,21 +2,23 @@ import { cons } from 'hexlet-pairs';
 import getRandomValue from '../random';
 import playGame from '..';
 
-const isPrimeValue = (number) => {
-  if (number <= 1) {
+const isPrime = (number) => {
+  if (number < 2) {
     return false;
   }
   for (let i = 2; i < number / 2; i += 1) {
-    if (number % i === 0) return false;
+    if (number % i === 0) {
+      return false;
+    }
   }
-  return number > 1;
+  return true;
 };
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getGameData = () => {
   const gameQuestion = getRandomValue(-10, 100);
-  const rightAnswer = isPrimeValue(gameQuestion) ? 'yes' : 'no';
+  const rightAnswer = isPrime(gameQuestion) ? 'yes' : 'no';
   return cons(gameQuestion, rightAnswer);
 };
 
