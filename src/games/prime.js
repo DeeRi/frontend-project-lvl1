7aa2d808a6fce0a1602601664e8369c8c7267a78
Alena Zueva <aleo.zueva@gmail.com>
2019-07-06 10:52:53 +0700

    naming changed

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,7 +3,10 @@ import getRandomValue from '../random';
 import playGame from '..';
 
 const isPrimeValue = (number) => {
-  for (let i = 2; i < number; i += 1) {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i < number / 2; i += 1) {
     if (number % i === 0) return false;
   }
   return number > 1;
@@ -12,7 +15,7 @@ const isPrimeValue = (number) => {
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getGameData = () => {
-  const gameQuestion = getRandomValue(1, 100);
+  const gameQuestion = getRandomValue(-10, 100);
   const rightAnswer = isPrimeValue(gameQuestion) ? 'yes' : 'no';
   return cons(gameQuestion, rightAnswer);
 };
