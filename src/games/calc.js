74dd6f123cf881f1420a31e95e2061abbfb04497
Alena Zueva <aleo.zueva@gmail.com>
2019-07-06 10:52:18 +0700

    list of operators and separate function to calculate expression added

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,32 +2,31 @@ import { cons } from 'hexlet-pairs';
 import getRandomValue from '../random';
 import playGame from '..';
 
+
+const mathOperators = ['+', '-', '*'];
+
+const calculateExpression = (a, b, operatorIndex) => {
+  switch (operatorIndex) {
+    case 0:
+      return a + b;
+    case 1:
+      return a - b;
+    case 2:
+      return a * b;
+    default:
+      return a + b;
+  }
+};
+
 const gameDescription = 'What is the result of the expression?';
 
 const getGameData = () => {
   const firstValue = getRandomValue(1, 100);
   const secondValue = getRandomValue(1, 100);
-  const operatorIndex = getRandomValue(1, 3);
-  let gameQuestion = '';
-  let expressionResult = 0;
-  switch (operatorIndex) {
-    case 1:
-      gameQuestion = `${firstValue} + ${secondValue}`;
-      expressionResult = Number(firstValue) + Number(secondValue);
-      break;
-    case 2:
-      gameQuestion = `${firstValue} - ${secondValue}`;
-      expressionResult = Number(firstValue) - Number(secondValue);
-      break;
-    case 3:
-      gameQuestion = `${firstValue} * ${secondValue}`;
-      expressionResult = Number(firstValue) * Number(secondValue);
-      break;
-    default:
-      console.log();
-  }
-  expressionResult = String(expressionResult);
-  return cons(gameQuestion, expressionResult);
+  const operatorIndex = getRandomValue(0, mathOperators.length - 1);
+  const gameQuestion = `${firstValue} ${mathOperators[operatorIndex]} ${secondValue}`;
+  const rightAnswer = String(calculateExpression(firstValue, secondValue, operatorIndex));
+  return cons(gameQuestion, rightAnswer);
 };
 
 export default () => playGame(gameDescription, getGameData);
