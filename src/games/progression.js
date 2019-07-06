4917adece1a1ef4d32268da9e354e412858836bf
Alena Zueva <aleo.zueva@gmail.com>
2019-07-06 10:56:37 +0700

    code restructed

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,24 +3,21 @@ import getRandomValue from '../random';
 import playGame from '..';
 
 const gameDescription = 'What number is missing in the progression?';
+const progressionLength = 10;
 
 const getGameData = () => {
-  let hiddenElement = 0;
-  let firstElement = 0;
-  let gameQuestion = '';
+  const firstElement = getRandomValue(1, 20);
   const step = getRandomValue(1, 100);
-  const hiddenElementIndex = getRandomValue(1, 10);
-  const progressionLength = 10;
-  for (let i = 1; i <= progressionLength; i += 1) {
-    firstElement += step;
-    const isSameIndex = i === hiddenElementIndex ? '..' : `${firstElement}`;
-    if (isSameIndex === '..') {
-      hiddenElement = firstElement;
-    }
+  const hiddenElementIndex = getRandomValue(0, progressionLength - 1);
+  const rightAnswer = String(firstElement + step * hiddenElementIndex);
+  let gameQuestion = '';
+
+  for (let i = 0; i < progressionLength; i += 1) {
+    const newElement = firstElement + step * i;
+    const isSameIndex = hiddenElementIndex === i ? '..' : newElement;
     gameQuestion = `${gameQuestion} ${isSameIndex}`;
   }
-  hiddenElement = String(hiddenElement);
-  return cons(gameQuestion, hiddenElement);
+  return cons(gameQuestion, rightAnswer);
 };
 
 export default () => playGame(gameDescription, getGameData);
